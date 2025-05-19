ad43ad753b5bb8a72fdcd9cff3f33e490ae2ea02
Tamilselvan-177 <231801177@rajalakshmi.edu.in>
2025-05-19 11:03:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Azure project"
-
+@app.route("/about")
+def about():
+    return "about section"
 if __name__ == "__main__":
     app.run()
